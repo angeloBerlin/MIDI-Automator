00c28e7dc5e837f4a3ecc14ba7619b953043fe28
Angelo Gülle <angelo@tieffrequent.de>
2018-01-21 16:58:08 +0100

    Open programs are not shown in focus ComboBox on Mac #59

---
+++ Midi Automator/build.sh
@@ -0,0 +1,13 @@
+# /usr/bin/hdiutil create -srcfolder "target/MIDI Automator.app" -volname "MIDI Automator" -ov "target/MIDI Automator-tmp.dmg" -format UDRW
+# /usr/bin/hdiutil attach "target/MIDI Automator-tmp.dmg" -mountroot /tmp
+# mkdir "/tmp/MIDI Automator/.background"
+# cp "images/DMGBackground.png" "/tmp/MIDI Automator/.background/background.png"
+# cp "images/MidiAutomatorIcon.icns" "/tmp/MIDI Automator/.VolumeIcon.icns"
+# cp "DS_Store" "/tmp/MIDI Automator/.DS_Store"
+# /usr/bin/SetFile -a C "/tmp/MIDI Automator"
+# ln -s "/Applications" "/tmp/MIDI Automator/Applications"
+# chmod +x "/tmp/MIDI Automator/MIDI Automator.app/Contents/PlugIns/jre/Contents/Home/jre/lib/jspawnhelper"
+# /usr/bin/hdiutil detach -force "/tmp/MIDI Automator"
+/usr/bin/hdiutil convert "target/MIDI Automator-tmp.dmg" -format UDZO -o "target/MIDI Automator.dmg"
+rm "target/MIDI Automator-tmp.dmg"
+
