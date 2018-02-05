1b17786b7210c7ca5be9f689d617694d65c76da3
Angelo Gülle <angelo@tieffrequent.de>
2018-02-05 23:17:28 +0100

    hdiutil convert results to “resource temporarily unavailable” #72

--- Midi Automator/build.sh
+++ Midi Automator/build.sh
@@ -1,3 +1,4 @@
+# 
 # /usr/bin/hdiutil create -srcfolder "target/MIDI Automator.app" -volname "MIDI Automator" -ov "target/MIDI Automator-tmp.dmg" -format UDRW
 # /usr/bin/hdiutil attach "target/MIDI Automator-tmp.dmg" -mountroot /tmp
 # mkdir "/tmp/MIDI Automator/.background"
@@ -8,6 +9,14 @@
 # ln -s "/Applications" "/tmp/MIDI Automator/Applications"
 # chmod +x "/tmp/MIDI Automator/MIDI Automator.app/Contents/PlugIns/jre/Contents/Home/jre/lib/jspawnhelper"
 # /usr/bin/hdiutil detach -force "/tmp/MIDI Automator"
-/usr/bin/hdiutil convert "target/MIDI Automator-tmp.dmg" -format UDZO -o "target/MIDI Automator.dmg"
+# diskutil eject "/tmp/MIDI Automator"
+/usr/bin/hdiutil convert "target/MIDI Automator-tmp.dmg" -format UDZO -o "target/MIDI Automator.dmg" -debug
 rm "target/MIDI Automator-tmp.dmg"
 
+
+cd /Users/aguelle/Documents/git/Midi\ Automator/Midi\ Automator
+/usr/bin/hdiutil create -srcfolder "target/MIDI Automator.app" -volname "MIDI Automator" -ov "target/MIDI Automator-tmp.dmg" -fs HFS+ -format UDRW
+/usr/bin/hdiutil attach "target/MIDI Automator-tmp.dmg" -mountroot /tmp
+/usr/bin/hdiutil detach -force "/tmp/MIDI Automator"
+sudo lsof | grep "MIDI Automator-tmp.dmg"
+/usr/bin/hdiutil convert "target/MIDI Automator-tmp.dmg" -format UDZO -o "target/MIDI Automator.dmg" -debug
